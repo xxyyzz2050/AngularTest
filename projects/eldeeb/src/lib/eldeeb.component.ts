70fc425cbb25db56ff909504b1e10061683e33c1
admin <admin@almogtama3.com>
2019-05-14 16:32:14 +0300

    init: eldeeb library

---
+++ projects/eldeeb/src/lib/eldeeb.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eldeeb-eldeeb',
+  template: `
+    <p>
+      eldeeb works!
+    </p>
+  `,
+  styles: []
+})
+export class EldeebComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
