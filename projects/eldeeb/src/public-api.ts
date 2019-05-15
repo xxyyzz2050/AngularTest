7abce533c9203386ca457c292d56560b5759378a
admin <admin@almogtama3.com>
2019-05-15 17:31:41 +0300

    eldeeb: classes general,files

--- projects/eldeeb/src/public-api.ts
+++ projects/eldeeb/src/public-api.ts
@@ -2,6 +2,9 @@
  * Public API Surface of eldeeb
  */
 
-export * from './lib/eldeeb.service';
-export * from './lib/eldeeb.component';
-export * from './lib/eldeeb.module';
+export * from "./lib/eldeeb.service";
+export * from "./lib/eldeeb.component";
+export * from "./lib/eldeeb.module";
+
+export * from "./lib/general";
+export * from "./lib/files"; //todo: if(platform.server)??
